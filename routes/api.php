189fb2632ab00e301ccd47d8b5b93b1917db2eb3
Curtis Delicata <curtis@liberugroup.com>
2026-08-29 18:58:02 +0100

    Synchronize maintenance module (#3)
    Co-authored-by: curtisdelicata <bot@liberu.software>

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,6 @@ Route::middleware('auth:sanctum')->prefix('api/v1/maintenance/reporting')->group
     Route::get('/{record}', [ReportingRecordController::class, 'show']);
     Route::patch('/{record}', [ReportingRecordController::class, 'update']);
     Route::delete('/{record}', [ReportingRecordController::class, 'destroy']);
+    Route::patch('/{record}', [ReportingRecordController::class, 'update']);
+    Route::delete('/{record}', [ReportingRecordController::class, 'destroy']);
 });
